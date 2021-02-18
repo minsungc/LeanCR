c1560f4800256d124f4e7cb1e253048c9e88b92d
minsungc <minsungc@andrew.cmu.edu>
2021-02-17 22:18:02 -0500

    Trying out some different things, not many changes to the original graphtheory.lean (although our cops and robbers game is now up and running!)

--- src/graphtheory_copy.lean
+++ src/graphtheory_copy.lean
@@ -82,13 +82,6 @@ def type_example (A B : Type) : Type := A × B × B
 
 example : my_foo_thingy.a = 4 := rfl
 
-#check foo
-
-variable x : foo
-
-#check x
-
-
 def adj' (V : Type) (u v : V) : Prop :=
 u = v
 
@@ -114,7 +107,9 @@ structure graph_hom {V W : Type} (G: refl_graph V) (H: refl_graph W) :=
 (to_fun : V → W)
 (mapEdges: ∀ v w, G.adj v w → H.adj (to_fun v) (to_fun w))
 
-structure cops_and_robbers_game :=
+#check graph_hom
+
+structure cr_game_init :=
 (V : Type)
 (G : refl_graph V)
 (num_cops : nat)
@@ -122,10 +117,14 @@ structure cops_and_robbers_game :=
 (cops_start : fin num_cops → V)
 (start_ok : ∀ i : fin num_cops, cops_start i ≠ robber_start)
 
+structure cr_game :=
+(I: cr_game_init)
+(cop_strat: list I.V  → list I.V)
+(robber_strat: list I.V → I.V)
 
 section CR_graphs
 
-parameters {V : Type u} (G : refl_graph V)
+parameters {V W: Type u} (G : refl_graph V)
 
 /-The definition of neighbor set was stolen from mathlib. Closed neighbor set is used to define corners, which is a central concept-/
 def neighbor_set (v : V) : set V := set_of (G.adj v)
@@ -141,22 +140,4 @@ A vtx w is a corner iff there exists some vertex v such that the neighbors of w 
 def corner_vtx (w: V) (S: set V) : Prop :=
   (∃ v ∈ S, closed_neighbor_set w ⊆ closed_neighbor_set v)
 
-
-/-
-Retracts: A graph homomorphism where a we send a corner to the vertex it is cornered by, and everything else is identity
-Setup: How should we define this? Thinking in terms of types, I was thinking something like Graph->Vertex->(Graph->Graph) (so it spits out a homomorphism), but it also might make sense to do something along the lines of Graph->Vertex->Graph (so it spits out the retracted graph).
--/
-def retract (G: refl_graph V) (v: V) : sorry
-
-/-
-Dismantlable Graph: A graph where a succession of retracts will always lead to K_1
-Needs some sort of recursive definition. Will look into this
--/
-def dismantlable_graph 
-
-/-
-Goal for the near future: Prove the following lemma: 
-Suppose there exists a dismantlable graph. Then the order in which the vertices are retracted form a partial order.
--/
-lemma dism_graph_has_partial_order
 end CR_graphs
--- src/graphtheory_withsets.lean
+++ src/graphtheory_withsets.lean
@@ -7,55 +7,55 @@ We define reflexive graphs as a reflexive symmetric relation on a vertex type 'V
 -/
 open finset
 
-structure refl_graph (V : Type) :=
-(adj : V → V → Prop) /-Is this the adjacency relation? Vertex-> Vertex-> -/
-(sym : symmetric adj . obviously)
+structure refl_graph (V: set Type) :=
+(adj : Type → Type → Prop) 
+(sym : ∀ x y ∈ V, adj x y ↔ adj y x . obviously)
 (selfloop : reflexive adj . obviously)
 
-def complete_refl_graph (V: Type) : refl_graph V :=
+def complete_refl_graph (V: set Type) : refl_graph V :=
 { adj := λ u v, true,
   sym := λ u v h, trivial,
   selfloop := λ u, trivial }
 
 /-Need to define graph homomorphism. Not defined yet-/
-structure graph_hom {V W : Type} (G: refl_graph V) (H: refl_graph W) :=
-(to_fun : V → W)
+structure graph_hom {V W : set Type} (G: refl_graph V) (H: refl_graph W) :=
+(to_fun : Type → Type)
 (mapEdges: ∀ v w, G.adj v w → H.adj (to_fun v) (to_fun w))
 
-#check graph_hom
-
 structure cr_game_init :=
-(V : Type)
+(V : set Type)
 (G : refl_graph V)
 (num_cops : nat)
-(robber_start : V)
+(robber_start : Type)
 (cops_start : fin num_cops → V)
-(start_ok : ∀ i : fin num_cops, cops_start i ≠ robber_start)
+/-Read into why this doesn't typecheck-/
+(start_ok : ∀ i : fin num_cops, robber_start ∉ cops_start i)
 
-/-
-Set up a cops and robber's game
--/
+/-Issue: Want the cop and robber strategies domain to be I.V^(num_cops+1)-/
 structure cr_game :=
 (I: cr_game_init)
 (cop_strat: vector I.V (I.num_cops+1)  → vector I.V (I.num_cops))
 (robber_strat: vector I.V (I.num_cops+1) → I.V)
-
 section CR_graphs
 
-parameters {V W: Type} (G : refl_graph V)
+parameters {V W: set Type} (G : refl_graph V)
 
 /-The definition of neighbor set was stolen from mathlib. Closed neighbor set is used to define corners, which is a central concept-/
-def neighbor_set (v : V) : set V := set_of (G.adj v)
-def neighbor_set' (v : V) : set V := { w | G.adj v w }
-
-
+def neighbor_set (v : Type) : set Type := set_of (G.adj v)
 def closed_neighbor_set (v : V) : set V := (neighbor_set v) ∪ {v}
 
 /-
 Corner vertices, an important concept in Cops and Robbers
 A vtx w is a corner iff there exists some vertex v such that the neighbors of w is a subset of the neighbors of v
 -/
-def corner_vtx (w: V) (S: set V) : Prop :=
+def corner_vtx (w: Type) (S: set Type) : Prop :=
   (∃ v ∈ S, closed_neighbor_set w ⊆ closed_neighbor_set v)
+
+/-May need to restructure some things. We want to be able to add/subtract vertices of our graph. Maybe set?-/
+structure retract (G: refl_graph V) (v: Type) (w: Type) :=
+{corner_cndn: corner_vtx v V}
+{retracted_graph: refl_graph V}
+{retract_hom: graph_hom G retracted_graph}
+
 
 end CR_graphs
