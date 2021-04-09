733a71548a299710b37194a4dd798902b3af8243
minsungc <minsungc@andrew.cmu.edu>
2021-04-08 23:44:26 -0400

    a lot of theorem proving attempts

--- src/graphtheory.lean
+++ src/graphtheory.lean
@@ -1,7 +1,8 @@
 import data.fintype.basic
 import data.sym2
 import data.list
-
+import order.conditionally_complete_lattice
+import system.random.basic
 
 
 /-
@@ -9,6 +10,7 @@ Reflexive Graphs:
 We define reflexive graphs as a reflexive symmetric relation on a vertex type 'V'.
 -/
 open finset
+open classical
 
 structure refl_graph (V: Type):=
 (adj : V → V → Prop) 
@@ -29,25 +31,6 @@ def empty_graph : refl_graph empty :=
   adj := λ u v, true,
   sym := λ u v h, trivial,
   selfloop := λ u, trivial
-}
-
-def k_cycle (n: ℕ ) : refl_graph (fin n) :=
-{
-  adj := λ x, λ y, (x=y) ∨ ↑y =↑x+1 ∨ ↑x =↑y+1 ∨ (↑y = n-1 ∧ ↑x=0) ∨ (↑x = n-1 ∧ ↑y=0),
-  sym :=
-  begin
-    intros u v,
-    intros h, 
-    cases h with h1 h2,
-    { }
-  end,
-  selfloop:=
-  begin
-    intro x,
-    have Q: x=x,
-      {refl,},
-    exact Q,
-  end
 }
 
 def cat_prod_graph {V W: Type} (G: refl_graph V) (H: refl_graph W) : refl_graph (V × W) :=
@@ -106,7 +89,7 @@ structure graph_iso {V W: Type} (G  : refl_graph V) (H: refl_graph W) :=
 (bij: function.bijective to_fun)
 (mapEdges: ∀ v w, G.adj v w ↔ H.adj (to_fun v) (to_fun w))
 
-def equivgraph {V W: Type} (G  : refl_graph V) (H: refl_graph W)  : Prop := ∃ a, a = graph_iso G H
+def equivgraph {V W: Type} (G  : refl_graph V) (H: refl_graph W)  : Prop := nonempty(graph_iso G H)
 
 notation G `≅` H  := equivgraph G H
 
@@ -137,62 +120,146 @@ def identity_iso {V: Type} (G: refl_graph V) : graph_iso G G:=
 }
 def trivial_iso : graph_iso empty_graph empty_graph := identity_iso empty_graph
 
-lemma isos_are_comm {V W: Type} {G  : refl_graph V} {H: refl_graph W} : cat_prod_graph G H ≅ cat_prod_graph H G :=
-begin
-  sorry,
-  /-TODO-/
-end
-
-variable n: ℕ
 /-
 Set up a cops and robber's game on a graph
 
 MAYBE separate the cop and robber strategy? Would be easier doing the universal quantifiers in the long run
 -/
-structure cop_strat {V: Type} [fintype V] (G: refl_graph V) (n : fin (fintype.card V+1)) :=
+structure cop_strat {V: Type} [fintype V] (G: refl_graph V) (n :ℕ ) :=
 (cop_init:  vector V n)
 (cop_strat: vector V n × V  → vector V n)
 (cop_legal: ∀ i v P, G.adj (vector.nth P i) (vector.nth (cop_strat (P,v)) i))
 
-structure rob_strat {V: Type} [fintype V] (G: refl_graph V) (n : fin (fintype.card V+1)) :=
+structure rob_strat {V: Type} [fintype V] (G: refl_graph V) (n : ℕ ) :=
 (rob_init: vector V n → V)
 (rob_strat: vector V n× V → V)
 (rob_legal: ∀ v P, G.adj v (rob_strat (P,v)))
 
-def capture {V: Type} [fintype V] {k : fin (fintype.card V+1)} (P: vector V k × V) := ∃ i, vector.nth P.1 i = P.2
+def capture {V: Type} [fintype V] {k : ℕ } (P: vector V k × V) := ∃ i, vector.nth P.1 i = P.2
 
-def round {V: Type} [fintype V] {G: refl_graph V} {k : fin (fintype.card V+1)} (CS: cop_strat G k) (RS: rob_strat G k) : ℕ → vector V k × V
+def round {V: Type} [fintype V] {G: refl_graph V} {k : ℕ } (CS: cop_strat G k) (RS: rob_strat G k) : ℕ → vector V k × V
 | 0 := (CS.cop_init, RS.rob_init (CS.cop_init))
 | (n+1) := (CS.cop_strat (round n), 
             RS.rob_strat(CS.cop_strat (round n), (round n).2) )
 
-def winning_strat_cop {V: Type} [fintype V] {G: refl_graph V} {k : fin (fintype.card V+1)} 
+def winning_strat_cop {V: Type} [fintype V] {G: refl_graph V} {k :ℕ } 
 (CS: cop_strat G k) := 
   ∀ RS: rob_strat G k , ∃ n : ℕ, capture (round CS RS n)
 
-def k_cop_win {V: Type} [fintype V] (G: refl_graph V) (k: fin (fintype.card V+1)) := 
+def k_cop_win {V: Type} [fintype V] (G: refl_graph V) (k: ℕ ) := 
 ∃ CS: cop_strat G k, winning_strat_cop CS
 
-def winning_strat_rob {V: Type} [fintype V] {G: refl_graph V} {k : fin (fintype.card V+1)} 
+def winning_strat_rob {V: Type} [fintype V] {G: refl_graph V} {k : ℕ } 
 (RS: rob_strat G k) := 
-  ∀ CS: cop_strat G k, not (∃ n : ℕ, capture (round CS RS n))
+  ∀ CS: cop_strat G k, ∀ n : ℕ, not(capture (round CS RS n))
 
-structure cr_game {V: Type} [fintype V] (G: refl_graph V) (n : fin (fintype.card V+1)):=
+structure cr_game {V: Type} [fintype V] (G: refl_graph V) (n : ℕ ):=
 (cop_strat: cop_strat G n)
 (rob_strat: rob_strat G n)
-(someone_wins: xor (winning_strat_cop cop_strat) (winning_strat_rob rob_strat))
+
 
 def cop_number {V: Type} [fintype V] [has_Inf ℕ] (G: refl_graph V) := 
   Inf {k : ℕ | k_cop_win G k}
 
 def cop_win_graph {V: Type} [fintype V] [has_Inf ℕ] (G: refl_graph V) := cop_number G = 1
 
-lemma zero_cops_cant_win {V: Type} [fintype V] [has_Inf ℕ] :
-  V ≠ empty → ∀ G : refl_graph V, cop_number G > 0 :=
+---------------------------------------------------------------------------------------------------------
+#check eq.symm
+noncomputable def exists_list_of_elts (V: Type) [fintype V] [decidable_eq V]: vector V (fintype.card V):= 
+begin
+  choose L hL using fintype.exists_univ_list V,
+  cases hL with h1 h2,
+  have P: V ≃ (fin L.length),
+    exact fintype.equiv_fin_of_forall_mem_list h2 h1,
+  have Q: fintype.card (fin L.length) = fintype.card V,
+    exact fintype.card_congr (equiv.symm P),
+  have R: fintype.card (fin L.length) = L.length,
+    exact fintype.card_fin (L.length),
+  have S: fintype.card V = L.length,
+    exact eq.trans (eq.symm Q) R,
+  exact ⟨L, eq.symm S⟩,
+end
+
+def find_index {V: Type} (p : V → Prop) [decidable_pred p] : list V → ℕ → option ℕ 
+| [] n    := none
+| (a::l) n := if p a then some n else find_index l (n+1)
+
+noncomputable def find_ind_vec {V: Type } [fintype V] [decidable_eq V] {n : ℕ } (vec: vector V n) (v: V) : ℕ :=
+begin
+  let L := vector.to_list vec,
+  let p := λ x, x=v,
+  let O := find_index p L 0,
+  have S : option.is_some O = tt,
+  {
+    /-TODO-/
+    sorry,
+  },
+  exact option.get S,
+end
+
+noncomputable def trivial_strategy {V: Type} [fintype V] [decidable_eq V] [has_Inf ℕ] (G: refl_graph V) : cop_strat G (fintype.card V) :=
+{
+  cop_init := exists_list_of_elts V,
+  cop_strat:= λ x, x.1,
+  cop_legal :=
+    begin
+      intros i v L,
+      simp,
+      apply G.selfloop,
+    end,
+}
+
+lemma lots_of_cops {V: Type} [fintype V] [has_Inf ℕ] [decidable_eq V] (G: refl_graph V) : 
+V ≠ empty  → set.nonempty {k : ℕ | k_cop_win G k} :=
+begin
+  intro non,
+  let strat_all := trivial_strategy G,
+  have con: (fintype.card V) ∈ {k : ℕ | k_cop_win G k},
+  {
+    use strat_all,
+    rw winning_strat_cop,
+    intro RS,
+    use 0,
+    rw capture,
+    /-TODO-/
+    sorry,
+  },
+  rwa set.nonempty_def,
+  use fintype.card V,
+  exact con,
+end
+
+#check nat.Inf_eq_zero.1
+lemma zero_cops_cant_win {V: Type} [fintype V] [has_Inf ℕ] [decidable_eq V] :
+  V ≠ empty → ∀ G : refl_graph V, 0<cop_number G :=
 begin
   intro non,
   intro G,
   by_contradiction H,
+  rw not_lt at H,
+  have zero_cops: cop_number G = 0,
+  {
+    have h: cop_number G ≤ 0 → cop_number G = 0,
+      simp,
+    exact h H,
+  },
+  rw cop_number at zero_cops,
+  have Inf_zero : 0 ∈ {k : ℕ | k_cop_win G k} ∨ {k : ℕ | k_cop_win G k} = ∅,
+  {
+    sorry,
+  },
+  cases Inf_zero with H1 H2,
+  {
+    sorry,
+  },
+  {
+    let K := lots_of_cops G non,
+    have negK : ¬ {k : ℕ | k_cop_win G k}.nonempty,
+    {
+      sorry,
+    }
+  },
+  -- apply (nat.Inf_eq_zero).1 zero_cops,
   /-Proof outline: 
   Suppose cop number of G=0. 
   Then G has a winning strategy with 0 cops.
@@ -203,10 +270,56 @@ begin
   -/
 end
 
-lemma complete_refl_graph_cop_win (V: Type) [fintype V] [has_Inf ℕ] : cop_win_graph (complete_refl_graph V) :=
+def complete_strategy (V: Type) [fintype V] [decidable_eq V] [inhabited V] [has_Inf ℕ] : cop_strat (complete_refl_graph V) 1 :=
+{
+  cop_init := 
+  begin
+  let v := arbitrary V,
+    let L := [v],
+  have prop: L.length = 1,
+  {
+    simp,
+  },
+  exact ⟨L, prop⟩, 
+  end,
+  cop_strat:= 
+  begin
+    intro x,
+    let L := [x.2],
+    have prop: L.length = 1,
+    {
+      simp,
+    },
+    exact ⟨L, prop⟩, 
+  end, 
+  cop_legal := 
+  begin
+    intros i v P,
+    simp,
+  end
+}
+
+lemma complete_refl_graph_cop_win (V: Type) [fintype V][decidable_eq V] [inhabited V]  [has_Inf ℕ] : V ≠ empty → cop_win_graph (complete_refl_graph V) :=
 begin
   rw cop_win_graph,
   rw cop_number,
+  intro non,
+  have CW : 1 ∈ {k : ℕ | k_cop_win (complete_refl_graph V) k},
+  {
+    let CW_strat := complete_strategy V,
+    have winning : winning_strat_cop CW_strat,
+    {
+      intro RS,
+      use 1,
+      use 0,
+      unfold round,
+      simp,
+      sorry,
+    },
+    simp,
+    use CW_strat,
+    exact winning,
+  },
   /-
   Proof outline:
   We want to find a winning cop strategy for a complete graph.
@@ -220,6 +333,7 @@ begin
   -/
 end
 
+---------------------------------------------------------------------------------------------------
 section CR_graphs
 
 parameters {V W: Type}[fintype V] (G : refl_graph V)
--- src/graphtheory_copy.lean
+++ src/graphtheory_copy.lean
@@ -1,143 +1,213 @@
 import data.fintype.basic
 import data.sym2
+import data.list
 
-namespace my_section
 
-variable x : nat
+def k_cycle (n: ℕ ) : refl_graph (fin n) :=
+{
+  adj := λ x, λ y, (x=y) ∨ ↑y =↑x+1 ∨ ↑x =↑y+1 ∨ (↑y = n-1 ∧ ↑x=0) ∨ (↑x = n-1 ∧ ↑y=0),
+  sym :=
+  begin
+    intros u v,
+    intros h, 
+    cases h with h1 h2,
+    { }
+  end,
+  selfloop:=
+  begin
+    intro x,
+    have Q: x=x,
+      {refl,},
+    exact Q,
+  end
+}
 
-def bar : nat := x + 3
-
-#check bar
-
-end my_section
-
-#check my_section.bar
-
-open my_section
-
-#check bar
-
-section
-
-variable x : list nat
-
-def baz := x ++ x 
-
-end
 
 /-
 Reflexive Graphs:
 We define reflexive graphs as a reflexive symmetric relation on a vertex type 'V'.
 -/
 open finset
-universe u
 
-variables x y z : nat
-
-def f : nat := x + 3
-
-#eval f 2
-
-def g := λ x, x + 3
-
-example : f = g := rfl
-
-#print f
-
-def adj {V : Type} : V → V → Prop :=
-λ u v, true
-
-example : adj 2 3 :=
-begin
-  dsimp [adj],
-  triv
-end
-
-example : adj 2 3 := trivial
-
-#check nat
-#check 2
-#check (2 : nat)
-
-
-structure foo (A : Type) : Type :=
-(a : nat)
-(b : nat)
-(c : A)
-(h : a + 3 < b)
-
-def my_foo_thingy : foo nat :=
-{ a := 4,
-  b := 9,
-  c := 3,
-  h := by norm_num }
-
-def type_example (A B : Type) : Type := A × B × B
-
-#check foo nat
-#check type_example nat nat
-
-#check my_foo_thingy
-#print my_foo_thingy
-
-example : my_foo_thingy.a = 4 := rfl
-
-def adj' (V : Type) (u v : V) : Prop :=
-u = v
-
-/-
-begin
-  intros u v,
-  exact u = v
-end 
--/
-
-structure refl_graph (V : Type u) :=
-(adj : V → V → Prop) /-Is this the adjacency relation? Vertex-> Vertex-> -/
-(sym : symmetric adj . obviously)
+structure refl_graph (V: Type):=
+(adj : V → V → Prop) 
+(sym : symmetric adj)
 (selfloop : reflexive adj . obviously)
 
-def complete_refl_graph (V: Type u) : refl_graph V :=
+
+#check refl_graph.sym
+
+def complete_refl_graph (V: Type) : refl_graph V :=
 { adj := λ u v, true,
-  sym := λ u v h, trivial,
+  sym := λ u v h,  trivial ,
   selfloop := λ u, trivial }
 
+
+def empty_graph : refl_graph empty :=
+/-complete_refl_graph empty-/
+{
+  adj := λ u v, true,
+  sym := λ u v h, trivial,
+  selfloop := λ u, trivial
+}
+
+def cat_prod_graph {V W: Type} (G: refl_graph V) (H: refl_graph W) : refl_graph (V × W) :=
+{
+  adj := λ u v, (G.adj u.1 v.1) ∧ (H.adj u.2 v.2),
+  sym :=
+  begin
+    intros u v,
+    intros h,
+/-QUESTION: If we flip lines 43 and 44 we get error. Why?-/
+    cases h with Gedge Hedge,
+    split,
+    exact G.sym Gedge,
+    exact H.sym Hedge,
+  end,
+  selfloop:=
+  begin
+    intro x,
+    split,
+    exact G.selfloop x.1,
+    exact H.selfloop x.2,
+  end
+}
+
+def singleton_graph :refl_graph unit := complete_refl_graph unit
+
 /-Need to define graph homomorphism. Not defined yet-/
-structure graph_hom {V W : Type} (G: refl_graph V) (H: refl_graph W) :=
+structure graph_hom {V W: Type} (G  : refl_graph V) (H: refl_graph W) :=
 (to_fun : V → W)
 (mapEdges: ∀ v w, G.adj v w → H.adj (to_fun v) (to_fun w))
 
-#check graph_hom
+def graph_hom_comp {U V W : Type} {G: refl_graph U} {H: refl_graph V} {I: refl_graph W} 
+  (f: graph_hom G H) (g: graph_hom H I) : graph_hom G I :=
+{
+  to_fun := g.to_fun ∘ f.to_fun ,
+  mapEdges := 
+  begin
+    intros u v,
+    intro h,
+    have adj_H : H.adj (f.to_fun u) (f.to_fun v),
+      exact f.mapEdges u v h,
+    exact g.mapEdges (f.to_fun u) (f.to_fun v) adj_H,
+  end 
+}
 
-structure cr_game_init :=
-(V : Type)
-(G : refl_graph V)
-(num_cops : nat)
-(robber_start : V)
-(cops_start : fin num_cops → V)
-(start_ok : ∀ i : fin num_cops, cops_start i ≠ robber_start)
+def trivial_hom {V:Type} (G: refl_graph V) : graph_hom G G :=
+{
+  to_fun := λ x, x,
+  mapEdges :=
+  begin
+    intros u v h, 
+    exact h,
+  end
+}
 
-structure cr_game :=
-(I: cr_game_init)
-(cop_strat: list I.V  → list I.V)
-(robber_strat: list I.V → I.V)
+structure graph_iso {V W: Type} (G  : refl_graph V) (H: refl_graph W) :=
+(to_fun : V → W)
+(bij: function.bijective to_fun)
+(mapEdges: ∀ v w, G.adj v w ↔ H.adj (to_fun v) (to_fun w))
 
+def equivgraph {V W: Type} (G  : refl_graph V) (H: refl_graph W)  : Prop := ∃ a, a = graph_iso G H
+
+notation G `≅` H  := equivgraph G H
+
+def identity_iso {V: Type} (G: refl_graph V) : graph_iso G G:=
+{
+  to_fun:= λ x,x,
+  bij:= 
+    begin
+      unfold function.bijective,
+      split, 
+      {
+        intros x y,
+        intros h,
+        apply h,
+      },
+      {
+        intro x,
+        use x,
+      },
+    end,
+  mapEdges:=
+    begin
+      intros v w,
+      split,
+      { intro h, exact h},
+      { intro h, exact h}
+    end
+}
+def trivial_iso : graph_iso empty_graph empty_graph := identity_iso empty_graph
+
+lemma isos_are_comm {V W: Type} {G  : refl_graph V} {H: refl_graph W} : cat_prod_graph G H ≅ cat_prod_graph H G :=
+begin
+  /-TODO-/
+end
+
+/-
+Set up a cops and robber's game on a graph
+A cops and robbers game will take in a graph G and a natural number n (the number of cops)
+-/
+structure cr_game {V W: Type}[fintype V] (G: refl_graph V) (n: fin (fintype.card V)) :=
+{cop_strat: option (vector V n) → option V → option (vector V n)}
+{cop_: }
+{robber_strat: option (vector V n) → option V → option V}
 section CR_graphs
 
-parameters {V W: Type u} (G : refl_graph V)
+parameters {V W: Type}[fintype V] (G : refl_graph V)
 
+#check fintype.card V
 /-The definition of neighbor set was stolen from mathlib. Closed neighbor set is used to define corners, which is a central concept-/
 def neighbor_set (v : V) : set V := set_of (G.adj v)
 def neighbor_set' (v : V) : set V := { w | G.adj v w }
 
-
 def closed_neighbor_set (v : V) : set V := (neighbor_set v) ∪ {v}
-
 /-
 Corner vertices, an important concept in Cops and Robbers
 A vtx w is a corner iff there exists some vertex v such that the neighbors of w is a subset of the neighbors of v
 -/
-def corner_vtx (w: V) (S: set V) : Prop :=
-  (∃ v ∈ S, closed_neighbor_set w ⊆ closed_neighbor_set v)
+def corner_vtx (w: V)  : Prop :=
+  (∃ v , closed_neighbor_set w ⊆ closed_neighbor_set v)
+
+def has_corner (G: refl_graph V) : Prop :=
+  (∃ w , corner_vtx w)
+
+/-Question: Why is univ defined as a finset-/
+#check univ
+
+#check some
+def is_corner (v: V) (G: refl_graph V) : Prop := (has_corner G) ∧ corner_vtx v 
+def find_corner (G: refl_graph V) : option V := if ∃ v, is_corner v G then some v else none
+
+/-TODO: This theorem. It uses the "second to last" move of the cop, which might be a little hard to encode-/
+theorem cwg_has_corner [decidable_eq V] [has_Inf ℕ] (G: refl_graph V): cop_win_graph G → has_corner G := 
+begin
+  intro h,
+  sorry,
+end
+
+lemma edge_symm (u v : V) : G.adj u v ↔  G.adj v u := ⟨λ x, G.sym x, λ y, G.sym y⟩
+ 
+def rm_graph (c: V) (H: refl_graph V) : refl_graph {v:V//v ≠ c} :=
+{ adj := λ a b, H.adj a b, 
+  sym :=  λ a b h, H.sym h,
+  selfloop := 
+  begin
+    intro a, 
+    apply H.selfloop,
+  end  
+}
+
+structure retract (c:V) (v:V) (H: refl_graph V):=
+(f: graph_hom H (rm_graph c H)) 
+(is_retract:  ↑(f.to_fun c) = v )
+
+def dismantle (G: refl_graph V) : list V → Prop
+| [] := G ≅ singleton_graph 
+| (a::L) := (is_corner a G) ∧ dismantle L
+
+def dismantlable_graph (G: refl_graph V) := ∃ L, dismantle G L
+
 
 end CR_graphs
